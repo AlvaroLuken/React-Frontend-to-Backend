e87872e9ba9533d2a275ada8055e5df4d745278d
AlvaroLuken <al5031@nyu.edu>
2021-09-15 10:02:59 -0700

    add

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ethers } from "ethers";
 import './App.css';
 import styled from "styled-components";
-import { Text } from "@chakra-ui/react"
+import { Input, Text, Center, Stack } from "@chakra-ui/react"
 
 const InputBox = styled.div`
   margin: 2rem;
@@ -37,22 +37,34 @@ export default function App() {
         </div>
 
         <div className="sub-header" styles="margin-bottom: 32px;">
-        Hello, I am input located in App.js!
+        Hello, I am a string on line 40 in App.js!
         </div>
         <div styles="height:32px"></div>
         
 
         <form onSubmit={null} styles="margin-top: 32px;">
-          <Text fontSize="6xl">This is a good way to send messages from front-end to back-end</Text>
+          <Center>
+            <Text fontSize="32px" align="center">This is a good way to send messages from front-end to back-end</Text>
+          </Center>
           <div styles="height:8px"></div>
           <label>
             1. Type a message to send to the server here...
-            <input id="message" type="text"/>
+            <Input id="message" type="text"/>
           </label>
         </form>
         <button className="button" onClick={myFunction}>
           2. Send the message to the back-end through this button's onClick
         </button>
+        <Center>
+          <Stack>
+            <ul>
+              <li><Text fontSize="6xl">After clicking, check the browser console to see the delivered message.</Text></li>
+              <li><Text fontSize="6xl">Logging of message comes from myFunction in App.js</Text></li>
+            </ul>
+          </Stack>
+        </Center>
+        
+
 
        
 
